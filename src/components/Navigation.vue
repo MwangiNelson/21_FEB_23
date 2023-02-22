7212e507b7c64d13e586a0603da5065dde284d10
MwangiNelson <nelsonmwangi197@gmail.com>
2023-02-22 09:40:26 +0300

    start

---
+++ src/components/Navigation.vue
@@ -0,0 +1,42 @@
+<script>
+    import ProfileVue from './ProfileVue.vue';
+</script>
+<template>
+  <nav class="app-navbar">
+    <div class="nav-links">
+      <a href="">HOME</a>
+      <a href="">ABOUT</a>
+      <a href="">PROFILE</a>
+    </div>
+    <div class="nav-profile">
+      <ProfileVue />
+    </div>
+  </nav>
+</template>
+
+
+<style scoped>
+.app-navbar {
+  width: 100%;
+  height: fit-content;
+  background-color: blueviolet;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: fixed;
+  top: 0;
+  left: 0;
+}
+.nav-links {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  justify-items: center;
+  width: 50%;
+  color: white;
+  text-transform: uppercase;
+}
+.nav-profile {
+  position: relative;
+  right: 10%;
+}
+</style>
---
+++ src/components/ProfileVue.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="profile-photo">
+    <img :src="ProfilePhoto" alt="" />
+  </div>
+</template>
+
+<script>
+import ProfilePhoto from "../assets/hackerman.jpg";
+export default {
+  data() {
+    return {
+      ProfilePhoto,
+    };
+  },
+};
+</script>
+
+<style scoped>
+.profile-photo img{
+  width: 5vw;
+  height: 5vw;
+  border-radius: 50%;
+object-fit: cover;
+}
+</style>
